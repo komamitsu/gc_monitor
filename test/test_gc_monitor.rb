3dd82577946646b72f7e9ab602357e7ea130c438
Komamitsu <komamitsu@gmail.com>
2009-08-09 23:56:50 +0900

    add a test checks only if it exits normal.

--- test/test_gc_monitor.rb
+++ test/test_gc_monitor.rb
@@ -1,11 +1,26 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
+class Foo
+  attr_accessor :dummy
+  include GcMonitor
+end
+
 class TestGcMonitor < Test::Unit::TestCase
 
   def setup
   end
   
-  def test_truth
-    assert true
+  def test_exit_normal
+    Foo.release_hook('puts "i am released."')  # This is option hook, for debug.
+    # GcMonitor.tcp_server('0.0.0.0', 4321)
+
+    10.times do
+      o = Foo.new
+      o.dummy = 'x' * 10 * 1024 * 1024  # For GC.
+      sleep 0.5
+    end
+
+    # You can filter the objects with using :time keyword.
+    assert GcMonitor.list(:time => 8).kind_of? Array
   end
 end
